3e9980857b52c2e8f122827f2f9d34c1032ce91c
Trey Kemp <kemptrey@outlook.com>
2022-04-28 17:47:43 -0600

    working on script

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const fill = document.querySelector(".fill");
 const empties = document.querySelectorAll(".empty");
 
-fill.addEventListener('dragstart', dragstart)
-fill.addEventListener('.dragend', dragend)
+fill.addEventListener("dragstart", dragstart);
+fill.addEventListener(".dragend", dragend);
 
-for(const empty of empties)
+for (const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+}
